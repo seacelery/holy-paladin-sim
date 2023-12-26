9841cfd585dfb2e10e7f76c83a670565896ae6e6
Amelia <seaceleries@gmail.com>
2023-12-26 07:22:23 +0000

    formatting

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes import main as main_blueprint
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(main_blueprint)
     return app
