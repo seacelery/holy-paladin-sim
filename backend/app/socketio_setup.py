57ac225760cb0561be72167010bba8bdb8e6a13e
Amelia <seaceleries@gmail.com>
2024-04-13 21:01:57 +0100

    adjust ping timeout for eventlet

--- backend/app/socketio_setup.py
+++ backend/app/socketio_setup.py
@@ -1,3 +1,3 @@
 from flask_socketio import SocketIO
 
-socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
+socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet", ping_timeout=600, ping_interval=300)
