14e9f1659c36eebfd6fd6b2f4f09e387e801f1bb
Amelia <seaceleries@gmail.com>
2024-04-13 02:32:59 +0100

    edit routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,10 @@
 import os
+
+from flask_socketio import SocketIO
 from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO
 import redis
 from app.routes import main as main_blueprint
-import logging
 
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='../../docs')
@@ -12,13 +12,37 @@ def create_app():
     app.redis = redis.Redis.from_url(app.config['REDIS_URL'])
     
     app.secret_key = os.getenv("FLASK_SECRET_KEY", "default_secret_key")
-    logging.basicConfig(level=logging.DEBUG)
-    app.logger.setLevel(logging.DEBUG)
     
-    CORS(app, supports_credentials=True)
+    # Set up CORS globally for all routes
+    CORS(app, supports_credentials=True, origins=['https://seacelery.github.io'])
+
+    # Initialize SocketIO with CORS allowed for the client's domain
+    socketio = SocketIO(app, cors_allowed_origins="https://seacelery.github.io")
+    
     app.register_blueprint(main_blueprint)
-    
-    socketio = SocketIO(app)
     socketio.init_app(app)
     
     return app
+
+# import os
+
+# from flask import Flask
+# from flask_cors import CORS
+# from .socketio_setup import socketio
+# from app.routes import main as main_blueprint
+
+# flask_secret_key = os.getenv("FLASK_SECRET_KEY")
+
+# import logging
+
+# logging.basicConfig(level=logging.DEBUG)
+
+# def create_app():
+#     app = Flask(__name__, static_url_path='', static_folder='../../docs')
+#     app.logger.setLevel(logging.DEBUG)
+#     app.secret_key = flask_secret_key
+#     CORS(app, supports_credentials=True)
+#     app.register_blueprint(main_blueprint)
+    
+#     socketio.init_app(app)
+#     return app
