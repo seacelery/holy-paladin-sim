29cd2a23c09ccfafc65e3e37658e45a47fa5987e
Amelia <seaceleries@gmail.com>
2024-04-13 02:12:44 +0100

    add old routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,20 +1,24 @@
 import os
-from flask import Flask, session
+from flask import Flask
 from flask_cors import CORS
+from flask_socketio import SocketIO
 import redis
-from .socketio_setup import socketio
 from app.routes import main as main_blueprint
 import logging
 
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='../../docs')
-    app.secret_key = os.getenv("FLASK_SECRET_KEY", "default_secret_key")
-    app.config['SESSION_REDIS'] = redis.from_url(os.environ.get('REDIS_URL', 'redis://localhost:6379'))
+    app.config['REDIS_URL'] = os.getenv('REDIS_URL', 'redis://localhost:6379')
+    app.redis = redis.Redis.from_url(app.config['REDIS_URL'])
     
+    app.secret_key = os.getenv("FLASK_SECRET_KEY", "default_secret_key")
+    logging.basicConfig(level=logging.DEBUG)
     app.logger.setLevel(logging.DEBUG)
+    
     CORS(app, supports_credentials=True)
     app.register_blueprint(main_blueprint)
     
+    socketio = SocketIO(app)
     socketio.init_app(app)
-
+    
     return app
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -18,31 +18,6 @@ default_priority_list = [
     ("Judgment | Infusion of Light duration < 5"),
 ]
 
-@main.route('/login', methods=['POST'])
-def login():
-    user_id = request.form['username']  # This is just an example
-    session_token = str(uuid.uuid4())
-    session_redis = current_app.config['SESSION_REDIS']
-
-    # Store user_id in Redis with session_token as the key
-    session_redis.setex(session_token, 3600, user_id)  # Expires in 3600 seconds (1 hour)
-
-    response = jsonify({'message': 'You are logged in!'})
-    response.set_cookie('session_id', session_token)
-    return response
-
-@main.route('/logout', methods=['GET'])
-def logout():
-    session_id = request.cookies.get('session_id')
-    session_redis = current_app.config['SESSION_REDIS']
-    
-    if session_id:
-        session_redis.delete(session_id)
-    
-    response = jsonify({'message': 'You are logged out!'})
-    response.set_cookie('session_id', '', expires=0)  # Clear the session_id cookie
-    return response
-
 @socketio.on("my event")
 def handle_my_custom_event(json):
     print("received json: " + str(json))
@@ -72,7 +47,6 @@ def cancel_simulation_route():
 
 @main.route("/import_character", methods=["GET"])
 def import_character_route():
-    session.clear()
     character_name = request.args.get("character_name")
     realm = request.args.get("realm")
     region = request.args.get("region")
@@ -81,14 +55,12 @@ def import_character_route():
     
     # paladin.reset_state()
     
-    session["character_name"] = character_name
-    session["realm"] = realm
-    session["region"] = region
+    session_token = str(uuid.uuid4())
+    modifiable_data = {"class_talents": {}, "spec_talents": {}, "race": "", "consumables": {}, "equipment": {}}
     
-    session["modifiable_data"] = {"class_talents": {}, "spec_talents": {}, "race": "", "consumables": {}, "equipment": {}}
-    current_app.logger.debug("Session initialized: %s", session["modifiable_data"])
+    current_app.redis.setex(session_token, 3600, json.dumps(modifiable_data))
 
-    return jsonify({
+    response = jsonify({
         "message": f"Character imported successfully, {character_name}, {realm}, {region}",
         "character_name": character_name,
         "character_realm": realm,
@@ -101,8 +73,11 @@ def import_character_route():
         "stats": {"haste": round(paladin.haste_rating), "crit": round(paladin.crit_rating), "mastery": round(paladin.mastery_rating), "versatility": round(paladin.versatility_rating), 
                   "intellect": round(paladin.spell_power), "health": round(paladin.max_health), "leech": round(paladin.leech_rating), "mana": round(paladin.max_mana),
                   "haste_percent": round(paladin.haste, 2), "crit_percent": round(paladin.crit, 2), "mastery_percent": round(paladin.mastery, 2), 
-                  "versatility_percent": round(paladin.versatility, 2), "leech_percent": round(paladin.leech, 2)}
+                  "versatility_percent": round(paladin.versatility, 2), "leech_percent": round(paladin.leech, 2)},
+        "session_token": session_token
     })
+    response.set_cookie('session_token', session_token)
+    return response
     
 @main.route("/fetch_updated_data", methods=["GET"])
 def fetch_updated_stats_route():
@@ -112,7 +87,17 @@ def fetch_updated_stats_route():
     custom_equipment = request.args.get("custom_equipment")
 
     paladin, healing_targets = import_character(character_name, realm, region)
-    modifiable_data = session.get("modifiable_data", {})
+    
+    session_token = request.cookies.get('session_token')
+    if not session_token:
+        return jsonify({"error": "No session token provided"}), 400
+
+    session_data = current_app.redis.get(session_token)
+    if not session_data:
+        return jsonify({"error": "Session not found"}), 404
+
+    modifiable_data = json.loads(session_data)
+    
     paladin.update_character(
         race=modifiable_data.get("race"),
         class_talents=modifiable_data.get("class_talents"),
@@ -140,38 +125,52 @@ def fetch_updated_stats_route():
 
 @main.route("/update_character", methods=["POST"])
 def update_character_route():
+    session_token = request.cookies.get('session_token')
+    if not session_token:
+        return jsonify({"error": "No session token provided"}), 400
+
+    session_data = current_app.redis.get(session_token)
+    if not session_data:
+        return jsonify({"error": "Session not found"}), 404
+
+    modifiable_data = json.loads(session_data)
     current_app.logger.debug("Session received in update_character: %s", session.get("modifiable_data"))
     user_input = request.json
     print("User Input:", user_input)
-    modifiable_data = session.get("modifiable_data", {})
     print("Modifiable Data before accessing 'spec_talents':", modifiable_data)  # Debugging
     
-    if "class_talents" in user_input:
-        for talent, value in user_input["class_talents"].items():
-            modifiable_data["class_talents"][talent] = value
+    # if "class_talents" in user_input:
+    #     for talent, value in user_input["class_talents"].items():
+    #         modifiable_data["class_talents"][talent] = value
 
-    if "spec_talents" in user_input:
-        for talent, value in user_input["spec_talents"].items():
-            modifiable_data["spec_talents"][talent] = value
+    # if "spec_talents" in user_input:
+    #     for talent, value in user_input["spec_talents"].items():
+    #         modifiable_data["spec_talents"][talent] = value
             
-    for item in user_input:
-        if item not in ["class_talents", "spec_talents"]:
-            modifiable_data[item] = user_input[item]
+    # for item in user_input:
+    #     if item not in ["class_talents", "spec_talents"]:
+    #         modifiable_data[item] = user_input[item]
             
-    print(modifiable_data)
-    session["modifiable_data"] = modifiable_data
-    log_session_size()
+    for key, value in user_input.items():
+        if key in modifiable_data:
+            modifiable_data[key].update(value)
+
+    # Save the updated data back to Redis
+    current_app.redis.setex(session_token, 3600, json.dumps(modifiable_data))
 
     return jsonify({"message": "Character updated successfully"})
 
 @main.route("/run_simulation", methods=["GET"])
 def run_simulation_route():
-    character_name = session.get("character_name")
-    realm = session.get("realm")
-    region = session.get("region")
+    session_token = request.cookies.get('session_token')
+    if not session_token:
+        return jsonify({"error": "No session token provided"}), 400
 
-    if not character_name or not realm:
-        return jsonify({"error": "Character name or realm not found in session"}), 400
+    session_data = current_app.redis.get(session_token)
+    if not session_data:
+        return jsonify({"error": "Session not found"}), 404
+
+    modifiable_data = json.loads(session_data)
 
     encounter_length = request.args.get("encounter_length", default=60, type=int)
     iterations = request.args.get("iterations", default=1, type=int)
@@ -190,11 +189,12 @@ def run_simulation_route():
     else:
         priority_list = default_priority_list
 
-    paladin, healing_targets = import_character(character_name, realm, region)
+    paladin, healing_targets = import_character(
+        modifiable_data['character_name'],
+        modifiable_data['realm'],
+        modifiable_data['region']
+    )
     
-    print(session["modifiable_data"])
-    
-    modifiable_data = session.get("modifiable_data", {})
     paladin.update_character(
         race=modifiable_data.get("race"),
         class_talents=modifiable_data.get("class_talents"),
