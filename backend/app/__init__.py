be5481ebd3c9549be0454ace136f90761f28f0f9
Amelia <seaceleries@gmail.com>
2024-04-13 02:52:52 +0100

    update cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,18 +5,20 @@ from flask_socketio import SocketIO
 import redis
 from app.routes import main as main_blueprint
 import logging
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='../../docs')
     app.config['REDIS_URL'] = os.getenv('REDIS_URL', 'redis://localhost:6379')
     app.redis = redis.Redis.from_url(app.config['REDIS_URL'])
-    
     app.secret_key = os.getenv("FLASK_SECRET_KEY", "super_secret_key")
     
     logging.basicConfig(level=logging.DEBUG)
     app.logger.setLevel(logging.DEBUG)
 
-    # Assuming the socketio setup is already imported and initialized in socketio_setup.py
+    # Update CORS setup here
+    CORS(app, resources={r"/*": {"origins": ["https://seacelery.github.io"]}}, supports_credentials=True)
+
     from app.socketio_setup import socketio
     socketio.init_app(app, cors_allowed_origins="https://seacelery.github.io")
 
