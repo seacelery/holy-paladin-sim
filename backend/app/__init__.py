7bbad8eaf60e2c39b8f85ee378ef38ec34e26fc3
Amelia <seaceleries@gmail.com>
2024-04-15 07:12:37 +0100

    debugging socket route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,17 @@
 import os
-from flask import Flask
+import json
+from flask import Flask, current_app
 import redis
 import certifi
 import pickle
 from app.routes import main as main_blueprint
+from app.main import import_character
 import logging
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from celery_config import make_celery
+
+from app.classes.run_simulation_task import run_simulation_task
 
 app = Flask(__name__, static_url_path="", static_folder="../../docs")
 app.config["REDIS_TLS_URL"] = os.getenv("REDIS_TLS_URL")
@@ -53,8 +57,53 @@ def register_socketio_events(socketio):
 
     @socketio.on('start_simulation')
     def handle_start_simulation(data):
-        print("Received start_simulation data:", data)
-        emit('simple_response', {'message': 'Simple check complete'})
+        session_token = data.get('session_token')
+        if not session_token:
+            emit('error', {"error": "No session token provided"})
+            return
+
+        session_data = current_app.redis.get(session_token)
+        if not session_data:
+            emit('error', {"error": "Session not found"})
+            return
+
+        modifiable_data = json.loads(session_data)
+
+        # Assuming import_character and other functions are accessible here
+        paladin, healing_targets = import_character(
+            modifiable_data['character_name'],
+            modifiable_data['realm'],
+            modifiable_data['region']
+        )
+
+        paladin.update_character(
+            race=modifiable_data.get("race"),
+            class_talents=modifiable_data.get("class_talents"),
+            spec_talents=modifiable_data.get("spec_talents"),
+            consumables=modifiable_data.get("consumables")
+        )
+
+        paladin_pickled = pickle.dumps(paladin)
+        healing_targets_pickled = pickle.dumps(healing_targets)
+
+        simulation_params = {
+            "paladin": paladin_pickled,
+            "healing_targets_list": healing_targets_pickled,
+            "encounter_length": int(data['encounter_length']),
+            "iterations": int(data['iterations']),
+            "time_warp_time": int(data['time_warp_time']),
+            "priority_list": data["priority_list"],
+            "custom_equipment": data["custom_equipment"],
+            "tick_rate": float(data['tick_rate']),
+            "raid_health": int(data['raid_health']),
+            "mastery_effectiveness": int(data['mastery_effectiveness']),
+            "light_of_dawn_targets": int(data['light_of_dawn_targets']),
+            "lights_hammer_targets": int(data['lights_hammer_targets']),
+            "resplendent_light_targets": int(data['resplendent_light_targets']),
+        }
+
+        result = run_simulation_task.delay(**simulation_params)
+        emit('simulation_started', {'message': "Simulation started successfully, monitor progress via WebSocket.", 'task_id': str(result.id)})
 
 @celery.task
 def process_paladin(paladin_data):
