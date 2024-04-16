ef4f43c70591b03d5de241f9a3e27dea6b38360b
Amelia <seaceleries@gmail.com>
2024-04-16 05:45:41 +0100

    update socket config

--- backend/app/socketio_setup.py
+++ backend/app/socketio_setup.py
@@ -3,4 +3,4 @@ from flask_socketio import SocketIO
 socketio = SocketIO(cors_allowed_origins="*")
 
 def init_socketio(app):
-    socketio.init_app(app, async_mode='eventlet', message_queue="rediss://:p07047fba795b7692e9c289c32b9129f04db91f5a51dadc7949bc932ea6d05bc0@ec2-34-250-232-88.eu-west-1.compute.amazonaws.com:10760", logger=True, engineio_logger=True)
+    socketio.init_app(app, async_mode='eventlet', message_queue="redis://:p07047fba795b7692e9c289c32b9129f04db91f5a51dadc7949bc932ea6d05bc0@ec2-34-250-232-88.eu-west-1.compute.amazonaws.com:10759", logger=True, engineio_logger=True)
