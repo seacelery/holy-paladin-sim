fb0c874f2b5ab199094e6717b4c5e0baae63ecec
Amelia <seaceleries@gmail.com>
2024-04-18 06:05:38 +0100

    debugging frontend config

--- docs/src/js/components/config.js
+++ docs/src/js/components/config.js
@@ -1,8 +1,8 @@
-const isDevelopment = window.location.hostname === 'localhost';
+const isDevelopment = window.location.hostname === 'http://127.0.0.1:5500/';
 console.log(isDevelopment)
 
 const CONFIG = {
-    backendUrl: isDevelopment ? 'http://localhost:5500' : 'https://holy-paladin-sim-6479e85b188f.herokuapp.com'
+    backendUrl: isDevelopment ? 'http://127.0.0.1:5500/' : 'https://holy-paladin-sim-6479e85b188f.herokuapp.com'
 };
 
 export { CONFIG };
