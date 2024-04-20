cc92b2424adf8659e81f6a703cfeb5584773cdab
Amelia <seaceleries@gmail.com>
2024-04-20 12:35:02 +0100

    remove some logs

--- docs/src/js/components/config.js
+++ docs/src/js/components/config.js
@@ -1,6 +1,4 @@
-const isDevelopment = window.location.hostname === 'localhost';
-console.log(window.location.hostname)
-console.log(isDevelopment)
+const isDevelopment = window.location.hostname === "localhost";
 
 const CONFIG = {
     backendUrl: isDevelopment ? "http://localhost:5000" : 'https://holy-paladin-sim-6479e85b188f.herokuapp.com'
