a8cde09cc38a7764d6a4bb90fe744fdba0a77b83
Amelia <seaceleries@gmail.com>
2024-04-21 08:22:15 +0100

    debug ptr toggle

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -11,6 +11,11 @@ from app.classes.simulation_state import cancel_simulation
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
 main = Blueprint("main", __name__)
 pp = pprint.PrettyPrinter(width=200)
