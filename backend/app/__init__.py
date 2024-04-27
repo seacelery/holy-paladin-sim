4390679fa3d9369bcc26beef87e84c16bd3b502e
Amelia <seaceleries@gmail.com>
2024-04-27 16:24:39 +0100

    update env

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -94,7 +94,7 @@ init_socketio(app)
 register_socketio_events(socketio)
 
 # if "DYNO" in os.environ:
-# os.environ['REDIS_TLS_URL'] = 'rediss://:p07047fba795b7692e9c289c32b9129f04db91f5a51dadc7949bc932ea6d05bc0@ec2-34-250-232-88.eu-west-1.compute.amazonaws.com:10760'
+
 
 app.config["REDIS_TLS_URL"] = os.getenv("REDIS_TLS_URL")
 app.redis = redis.Redis.from_url(
