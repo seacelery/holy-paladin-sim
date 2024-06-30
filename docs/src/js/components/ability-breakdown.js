0d0133dbbb43880c81719b8cd8cde73017b38300
Amelia <seaceleries@gmail.com>
2024-06-30 14:18:03 +0100

    add fading light

--- docs/src/js/components/ability-breakdown.js
+++ docs/src/js/components/ability-breakdown.js
@@ -125,7 +125,7 @@ const createAbilityBreakdown = (simulationData, containerCount) => {
         "Broodkeeper's Promise", "Sun Sear", "Sacred Weapon 1", "Sacred Weapon 2", "Authority of Fiery Resolve", "Rite of Adjuration",
         "Avenging Crusader (Judgment)", "Avenging Crusader (Crusader Strike)", "Sun's Avatar", "Divine Guidance", "Hammer and Anvil", "Scrapsinger's Symphony",
         "Siphoning Phylactery Shard", "Creeping Coagulum ", "Gruesome Syringe", "Truth Prevails", "Saved by the Light (Word of Glory)", 
-        "Saved by the Light (Eternal Flame)", "Saved by the Light (Light of Dawn)", "Radiant Aura", "Sacred Word"
+        "Saved by the Light (Eternal Flame)", "Saved by the Light (Light of Dawn)", "Radiant Aura", "Sacred Word", "Fading Light"
     ];
     // displays casts with average as healing divided by hits
     const excludedSpellsCastsAverageHits = [
@@ -135,7 +135,7 @@ const createAbilityBreakdown = (simulationData, containerCount) => {
         "Beacon of Light", "Overflowing Light", "Resplendent Light", "Crusader's Reprieve", "Crusader Strike", "Judgment", "Daybreak", 
         "Divine Toll", "Smoldering Seedling", "Blossom of Amirdrassil Absorb", "Blossom of Amirdrassil", "Lay on Hands", "Leech", "Veneration",
         "Light of the Martyr ", "Saved by the Light", "Dawnlight", "Broodkeeper's Promise", "Sacred Weapon", "Holy Bulwark",
-        "Avenging Crusader", "Siphoning Phylactery Shard"
+        "Avenging Crusader", "Siphoning Phylactery Shard", "Fading Light"
     ];
     const excludedSpellsAverage = [
         "Dawnlight"
